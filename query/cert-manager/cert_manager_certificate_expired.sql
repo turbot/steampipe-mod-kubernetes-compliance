551c7cd5ad9a28ce5c2eafffaa70987d4e130517
ParthaI <partha@turbot.com>
2022-09-22 14:00:16 +0800

    Update

--- query/cert-manager/cert_manager_certificate_expired.sql
+++ query/cert-manager/cert_manager_certificate_expired.sql
@@ -1,12 +1,12 @@
 select
   -- Required Columns
   uid as resource,
-  case when "spec_renewBefore" is not null and now() > creation_timestamp + (interval '1 hour' * NULLIF(left("spec_renewBefore", length("spec_duration")-2), '')::int) then
+  case when "spec_renewBefore" is not null and now() > creation_timestamp + (interval '1 hour' * NULLIF(left("spec_renewBefore", length("spec_renewBefore")-2), '')::int) then
     'alarm'
   else
     'ok'
   end as status,
-  case when "spec_renewBefore" is not null and now() > creation_timestamp + (interval '1 hour' * NULLIF(left("spec_renewBefore", length("spec_duration")-2), '')::int) then
+  case when "spec_renewBefore" is not null and now() > creation_timestamp + (interval '1 hour' * NULLIF(left("spec_renewBefore", length("spec_renewBefore")-2), '')::int) then
     name || ' certificate is expired.'
   else
     name || ' certificate is not expired.'
