b09272fff3ad12825f4e357fe90416691c0a190b
Subhajit Mondal <subhajit@turbot.com>
2023-04-07 21:50:41 +0530

    Update deployment queries

--- query/deployment/deployment_container_privilege_escalation_disabled.sql
+++ query/deployment/deployment_container_privilege_escalation_disabled.sql
@@ -1,6 +1,9 @@
 select
   -- Required Columns
-  name ||  '_' || namespace as resource,
+  case
+    when manifest_file_path is null then uid
+    else name ||  '_' || namespace || '-' || manifest_file_path
+  end as resource,
   case
     when c -> 'securityContext' ->> 'allowPrivilegeEscalation' = 'false' then 'ok'
     else 'alarm'
--- query/deployment/deployment_hostpid_hostipc_sharing_disabled.sql
+++ query/deployment/deployment_hostpid_hostipc_sharing_disabled.sql
@@ -1,6 +1,9 @@
 select
   -- Required Columns
-  name ||  '_' || namespace as resource,
+  case
+    when manifest_file_path is null then uid
+    else name ||  '_' || namespace || '-' || manifest_file_path
+  end as resource,
   case
     when template -> 'spec' ->> 'hostPID' = 'true' or template -> 'spec' ->> 'hostIPC' = 'true' then 'alarm'
     else 'ok'
